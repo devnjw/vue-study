4f891e04002242f6b1cc80a353cacff0f41ff3af
devnjw <devnjw@gmail.com>
2022-01-18 15:40:55 +0900

    component practice

---
+++ vue-component/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <global-component></global-component>
+    <local-component></local-component>
+  </div>
+</template>
+
+<script>
+  import LocalComponent from './components/local-component'
+
+  export default {
+    name: 'app',
+    components: { LocalComponent }
+  }
+</script>
