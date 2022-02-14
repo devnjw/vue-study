d243f8be5518f044f9eb14de722a9a5d19c752e3
devnjw <devnjw@gmail.com>
2022-02-14 17:20:52 +0900

    stepline chart practice

---
+++ chart-stepline/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="chart">
+    <apexchart type="line" height="350" :options="chartOptions" :series="series"></apexchart>
+  </div>
+</template>
+
+<script>
+import VueApexCharts from 'vue-apexcharts'
+
+export default {
+  name: 'App',
+  components: {
+    apexchart: VueApexCharts,
+  },
+  data:() => ({
+    
+    series: [{
+      data: [{ x: '05/06/2014', y: 54 }, { x: '05/08/2014', y: 17 } , { x: '06/01/2014', y: 26 }]
+    }],
+    chartOptions: {
+      chart: {
+        type: 'line',
+        height: 350
+      },
+      stroke: {
+        curve: 'stepline',
+      },
+      xaxis:{
+        type: 'datetime',
+      },
+      dataLabels: {
+        enabled: false
+      },
+      title: {
+        text: 'Stepline Chart',
+        align: 'left'
+      },
+      markers: {
+        hover: {
+          sizeOffset: 4
+        }
+      }
+    },
+  }),
+}
+</script>
+
+<style>
+</style>
